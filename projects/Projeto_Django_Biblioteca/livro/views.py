3e40d667365cd7d7e0692455681416aca5941c13
Leonardo Bonifácio <estudotrafego@outlook.com>
2024-05-06 20:04:00 -0300

    more code

--- projects/Projeto_Django_Biblioteca/livro/views.py
+++ projects/Projeto_Django_Biblioteca/livro/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from usuarios.models import  Usuario
 from .models import Livros,Categoria,Emprestimo
+from .forms import CadastroLivro
 # Create your views here.
 
 def home(request):
     if request.session.get('usuario'):
         usuario = Usuario.objects.get(id = request.session['usuario'])
         livros = Livros.objects.filter(usuario=usuario)
-        return render(request, 'home.html', {'livros':livros})
+        form = CadastroLivro()
+        return render(request, 'home.html', {'livros':livros, 'usuario_logado': request.session.get('usuario'), 'form':form})
     else:
         return redirect('/auth/login/?status=2')
 
@@ -19,7 +21,21 @@ def ver_livros(request, id):
         if request.session.get('usuario') == livro.usuario.id:
             categoria_livro = Categoria.objects.filter(usuario_id = request.session.get('usuario'))
             emprestimos = Emprestimo.objects.filter(livro=livro)
-            return render(request, 'ver_livro.html', {'livro':livro, 'categoria_livro':categoria_livro, 'emprestimos':emprestimos})
+            form = CadastroLivro()
+            return render(request, 'ver_livro.html', {'livro':livro, 'categoria_livro':categoria_livro, 'emprestimos':emprestimos,
+                                                       'usuario_logado': request.session.get('usuario'),
+                                                       'form':form})
         else:
             return HttpResponse('Esse livro não é seu')
     return redirect('/auth/login/?status=2')
+
+
+def cadastrar_livro(request):
+    if request.method == 'POST':
+        form = CadastroLivro(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Livro salvo com sucesso')
+        else:
+            return HttpResponse('Dados inválidos')
