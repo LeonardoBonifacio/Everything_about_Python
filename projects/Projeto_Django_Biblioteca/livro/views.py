2f1373c0d66486c5d50def135eaa6255c2357b5a
Leonardo Bonifácio <estudotrafego@outlook.com>
2024-05-07 20:15:50 -0300

    more

--- projects/Projeto_Django_Biblioteca/livro/views.py
+++ projects/Projeto_Django_Biblioteca/livro/views.py
@@ -10,6 +10,8 @@ def home(request):
         usuario = Usuario.objects.get(id = request.session['usuario'])
         livros = Livros.objects.filter(usuario=usuario)
         form = CadastroLivro()
+        form.fields['usuario'].initial = request.session['usuario']
+        form.fields['categoria'].queryset = Categoria.objects.filter(usuario = usuario)
         return render(request, 'home.html', {'livros':livros, 'usuario_logado': request.session.get('usuario'), 'form':form})
     else:
         return redirect('/auth/login/?status=2')
@@ -19,9 +21,12 @@ def ver_livros(request, id):
     if request.session.get('usuario'):
         livro = Livros.objects.get(id=id)
         if request.session.get('usuario') == livro.usuario.id:
+            usuario = Usuario.objects.get(id = request.session['usuario'])
             categoria_livro = Categoria.objects.filter(usuario_id = request.session.get('usuario'))
             emprestimos = Emprestimo.objects.filter(livro=livro)
             form = CadastroLivro()
+            form.fields['usuario'].initial = request.session['usuario']
+            form.fields['categoria'].queryset = Categoria.objects.filter(usuario = usuario)
             return render(request, 'ver_livro.html', {'livro':livro, 'categoria_livro':categoria_livro, 'emprestimos':emprestimos,
                                                        'usuario_logado': request.session.get('usuario'),
                                                        'form':form})
@@ -36,6 +41,6 @@ def cadastrar_livro(request):
         
         if form.is_valid():
             form.save()
-            return HttpResponse('Livro salvo com sucesso')
+            return redirect('/livro/home/')
         else:
             return HttpResponse('Dados inválidos')
